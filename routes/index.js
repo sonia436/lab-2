e403c762ec5e0aebf6490d882714be960c26c033
Your Name <you@example.com>
2019-05-06 17:27:11 +0600

    lab

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index');
+});
+router.get('/book', function(req, res) {
+  res.render('book');
+});
+
+module.exports = router;
